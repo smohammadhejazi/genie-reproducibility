b8ca23cf6422f5d6fc00d102737da9a19236e6f5
smohammadhejazi <smhejazihoseini@gmail.com>
2025-03-30 18:28:29 -0400

    load  mobilenet_v2

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import torch
 import torchvision.models as models
 
 resnet18 = models.resnet18(pretrained=True)
-print(resnet18)
-
 input_tensor = torch.randn(1, 3, 224, 224)
 output = resnet18(input_tensor)
+print(output)
+
+mobilenet_v2 = models.mobilenet_v2(pretrained=True)
+input_tensor = torch.randn(1, 3, 224, 224)
+output = mobilenet_v2(input_tensor)
 print(output)
