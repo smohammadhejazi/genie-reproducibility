b65fccbeb873a258de6b11716b043d257b25dd79
smohammadhejazi <smhejazihoseini@gmail.com>
2025-03-30 23:58:24 -0400

    add argument parser and organize stages of code

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import torch
+import sys
+import argparse
 import torchvision.models as models
+from torchvision.models import ResNet18_Weights, MobileNet_V2_Weights
+from distil import distill_data
+from quantize import quantize_model
 
-resnet18 = models.resnet18(pretrained=True)
-input_tensor = torch.randn(1, 3, 224, 224)
-output = resnet18(input_tensor)
-print(output)
 
-mobilenet_v2 = models.mobilenet_v2(pretrained=True)
-input_tensor = torch.randn(1, 3, 224, 224)
-output = mobilenet_v2(input_tensor)
-print(output)
+def main():
+
+    sys.argv = ["main.py", "resnet18", "-d", "-q"]
+    
+    prog = "Genie Reproducibility Challenge"
+    descr = "Distill data and quantize models"
+    parser = argparse.ArgumentParser(prog=prog, description=descr)
+    parser.add_argument("-d", "--distill", required=True, action="store_true", help="Distill images")
+    parser.add_argument("-q", "--quantize", required=True, action="store_true", help="Quantize model")
+    parser.add_argument("model", choices=["resnet18", "mobilenet_v2"], help="Model to use in distill and quantize")
+    args = parser.parse_args()
+
+    if args.model == "resnet18":
+        model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+    elif args.model == "mobilenet_v2":
+        model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
+
+    if args.distill:
+        distill_data(model)
+    if args.quantize:
+        quantize_model(model)
+
+
+if __name__ == "__main__":
+    main()
