66d190802bba8ccf01577a4b4a4b424b42cb3409
smohammadhejazi <smhejazihoseini@gmail.com>
2025-04-13 16:51:10 -0400

    update structure of main

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import torch
-import json
 import sys
 import argparse
 import torchvision.models as models
 from torchvision.models import ResNet18_Weights, MobileNet_V2_Weights
 from distil import distill_data
 from quantize import quantize_model
+# from quantize import reconstruct
+from evaluation import evaluate
 
 
 def main():
-
     sys.argv = ["main.py", "resnet18", "-d"]
-
-    # with open("config.json", "r") as f:
-    #     config = json.load(f)
-
     
     prog = "Genie Reproducibility Challenge"
     descr = "Distill data and quantize models"
@@ -30,9 +25,14 @@ def main():
         model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
 
     if args.distill:
-        distill_data(model)
+        train_dataset = distill_data(model)
     if args.quantize:
-        quantize_model(model)
+        quantized_model = quantize_model(model)
+    # if args.reconstruct:
+    #     reconstruct(quantize_model, model, train_dataset)
+    if args.evaluate:
+        evaluate(quantized_model)
+
 
 
 if __name__ == "__main__":
